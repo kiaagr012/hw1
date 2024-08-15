8ffc1cc7560deac95c5b1a39063a91fc9e836b8a
kiaagr012 <kianagrawal126@gmail.com>
2024-08-14 19:18:18 -0700

    Add files via upload

---
+++ main.py
@@ -0,0 +1,8 @@
+book = {'name':'the voyage of the dawn treader', 'author':'C.S Lewis','genre': 'fantasy fiction'}
+print(book)
+print(book['name'])
+print(book['author'])
+print(book['genre'])
+
+print(book.keys())
+print(book.values())
